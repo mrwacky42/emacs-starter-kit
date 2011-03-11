2044810614b7a99fef3f8e87d6816df8b6f1ef5c
Sharif Nassar <sharif@mrwacky.com>
2011-03-10 23:46:20 -0800

    Move my custom stuff to src, so it can be shared across hosts if need be.

--- init.el
+++ init.el
@@ -68,7 +68,7 @@
 ;; You can keep system- or user-specific customizations here
 (setq system-specific-config (concat dotfiles-dir system-name ".el")
       user-specific-config (concat dotfiles-dir user-login-name ".el")
-      user-specific-dir (concat dotfiles-dir user-login-name))
+      user-specific-dir (concat dotfiles-dir "src"))
 (add-to-list 'load-path user-specific-dir)
 
 (if (file-exists-p system-specific-config) (load system-specific-config))
