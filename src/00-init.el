b1afa9266d902d94d881305b391e512f1bda1f2d
Sharif Nassar <sharif@mrwacky.com>
2011-10-28 20:51:19 -0700

    Load my snips.

--- src/00-init.el
+++ src/00-init.el
@@ -282,3 +282,8 @@
 (require 'edit-server)
 (edit-server-start)
   
+;; Develop and keep personal snippets under ~/emacs.d/mysnippets
+(setq yas/root-directory "~/emacs.d/src/yasnips")
+
+;; Load the snippets
+(yas/load-directory yas/root-directory)
